5bce2ee5e4ec9a209c18efedc4fe0888a0d431b7
Suraj Pandey <surajpandey230522@gmail.com>
2026-07-20 17:47:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+
+let screen=document.querySelector("#screen")
+let button=document.querySelector("button")
+
+
+class password{
+   constructor(){
+      alert("welcome to password genarator")
+      this.pass=""
+   }
+   genaratePassword(len){
+    this.pass="";
+      let char="abcdefghijklmnopqrstyvwxyz"
+      let num="1234567890"
+      let special="!@#$%^&*()"
+   if(len<3){
+      console.log("your passwaord should be atleast 3 characters  long")
+   }
+   else{
+      let i=0;
+      while(i<len){
+         this.pass+= char[Math.floor(Math.random()*char.length)]
+         this.pass+= num[Math.floor(Math.random()*num.length)]
+         this.pass+= special[Math.floor(Math.random()*special.length)]
+         i+=3;
+      }
+      return this.pass
+      }
+
+   }
+}
+let p=new password()
+
+// console.log(p.genaratePassword(5))
+
+
+button.addEventListener("click", () => {
+    screen.innerText = p.genaratePassword(8);
+});
